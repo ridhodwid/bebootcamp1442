7affc60fb5bd3c0d6e5d578cc78aa844b7e62680
Ridho Dwi Dharmawan <ridhodwidh@gmail.com>
2021-04-16 10:38:58 +0700

    heroku publish

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ fastify.get('/sun', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(5000)
+    await fastify.listen(5000, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
