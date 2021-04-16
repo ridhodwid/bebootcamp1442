3ebb3a8d391f950712ac52f356adb4310291d07c
Ridho Dwi Dharmawan <ridhodwidh@gmail.com>
2021-04-16 08:38:16 +0700

    day2

---
+++ index.js
@@ -0,0 +1,23 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Declare a route
+fastify.get('/sun', async (request, reply) => {
+  return { hello: 'sun' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
